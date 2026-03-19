9f0425e94981ce7c989526c0a1d667a5ca739b64
accama <accamamuhammad@gmail.com>
2026-03-19 06:20:35 +0100

    clerk installation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-
+import { 
+  ClerkProvider, 
+  SignInButton, 
+  SignUpButton, 
+  Show, 
+  UserButton 
+} from "@clerk/nextjs";
 import { ThemeProvider } from "next-themes";
-
 import { Instrument_Serif, Plus_Jakarta_Sans } from "next/font/google";
-
 import "./globals.css";
 import { Metadata } from "next";
 
 const instrumentSerif = Instrument_Serif({
   weight: "400",
-
   subsets: ["latin"],
-
   variable: "--font-serif",
 });
 
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
-
   weight: ["300", "400", "500", "600"],
-
   variable: "--font-sans",
 });
 
@@ -37,11 +37,20 @@ export default function RootLayout({
       <body
         className={`${instrumentSerif.variable} ${plusJakarta.variable} font-sans`}
       >
+        <ClerkProvider>
           <ThemeProvider attribute="class" defaultTheme="light">
             <header className="flex justify-end items-center p-4 gap-4 h-16">
+              <Show when="signed-out">
+                <SignInButton mode="modal" />
+                <SignUpButton mode="modal" />
+              </Show>
+              <Show when="signed-in">
+                <UserButton />
+              </Show>
             </header>
             <main>{children}</main>
           </ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
