d49bda1dbc6057ca7a8555902e3a4fae0c0a38f6
accama <accamamuhammad@gmail.com>
2026-04-03 18:40:22 +0100

    error fix

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,8 @@
-// src/db.ts
-import 'dotenv/config'
-import { PrismaClient } from '../app/generated/prisma'
-import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from "@/app/generated/prisma";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL!,
-})
+});
 
-export const prisma = new PrismaClient({ adapter })
+export const prisma = new PrismaClient({ adapter });
