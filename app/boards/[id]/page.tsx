383c589b550c98e778ff513245c64ebff3e4fa11
accama <accamamuhammad@gmail.com>
2026-04-05 07:07:43 +0100

    remove clerk - error fix

--- app/boards/[id]/page.tsx
+++ app/boards/[id]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { notFound } from "next/navigation";
 import { getBoardById } from "@/actions/boards";
 import BoardDetailClient from "./BoardDetailClient";
--- app/starred/page.tsx
+++ app/starred/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { getBoards } from "@/actions/boards";
 import StarredClient from "./StarredClient";
 
